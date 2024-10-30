68a8369148f77761693fc6966b2832e1955f8b8e
Mayur Kamble <mayurkamble0250@gmail.com>
2024-10-30 09:30:25 +0530

    added mongoose and connected to mongo db

--- Index.js
+++ Index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 app.get("/welcome", (req, res) => {
   console.log(req);
   return res.status(200).send("Welcome");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
