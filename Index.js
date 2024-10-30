c82a11dbdb93b33f5ce59c5408c99a71b13a3def
Mayur Kamble <mayurkamble0250@gmail.com>
2024-10-30 09:23:21 +0530

    created first test route

--- Index.js
+++ Index.js
@@ -6,3 +6,8 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/welcome", (req, res) => {
+  console.log(req);
+  return res.status(200).send("Welcome");
+});
